9dd287fd983b9ab32a3ff9e01a930ab9400d500d
pman <pman-$-432894328923@gmail.com>
2024-08-21 15:10:20 +0200

    ...

--- helm-approach/cluster.sh
+++ helm-approach/cluster.sh
@@ -27,3 +27,12 @@ nodes:
 - role: control-plane
 - role: worker
 EOF
+
+helm upgrade --install --wait --timeout 15m \
+  --namespace monitoring --create-namespace \
+  --repo https://prometheus-community.github.io/helm-charts \
+  kube-prometheus-stack kube-prometheus-stack --values - <<EOF
+kubeEtcd:
+  service:
+    targetPort: 2381
+EOF

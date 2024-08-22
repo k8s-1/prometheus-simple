34db4a3604078e31491668a9250a7fdb577fac13
pman <pman-$-432894328923@gmail.com>
2024-08-22 15:51:37 +0200

    ...

--- run.sh
+++ run.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
+
+kind delete cluster && kind create cluster --config - <<EOF
+kind: Cluster
+apiVersion: kind.x-k8s.io/v1alpha4
+nodes:
+  - role: control-plane
+  - role: worker
+  - role: worker
+EOF
+kubectl cluster-info --context kind-kind
+
 kubectl apply --server-side -f bundle.yaml
 
 kustomize build . | kubectl apply --server-side -f -

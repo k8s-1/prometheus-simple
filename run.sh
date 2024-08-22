10ec438522b2253c47e4c2407bf37bf55f0cd948
pman <pman-$-432894328923@gmail.com>
2024-08-22 15:38:26 +0200

    ...

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@ kubectl apply --server-side -f bundle.yaml
 
 kustomize build . | kubectl apply --server-side -f -
 
-kubectl port-forward svc/prometheus-k8s 9090:9090 > /dev/null 2>&1 &
+kubectl port-forward svc/prometheus-operated 9090:9090 > /dev/null 2>&1 &
 
 printf "\nMetrics are being monitored:"
 url="http://localhost:9090/targets"

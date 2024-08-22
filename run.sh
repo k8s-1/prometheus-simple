708949c78a0a711aa5771222ce578214db52f47c
pman <pman-$-432894328923@gmail.com>
2024-08-22 15:50:48 +0200

    ...

--- run.sh
+++ run.sh
@@ -3,6 +3,7 @@ kubectl apply --server-side -f bundle.yaml
 
 kustomize build . | kubectl apply --server-side -f -
 
+sleep 5
 kubectl wait --for=condition=ready pod -l app.kubernetes.io/name=prometheus --timeout 300s
 
 kubectl port-forward svc/prometheus-operated 9090:9090 > /dev/null 2>&1 &

7801ca94430e1db1e5f3eaf6d82075d8261bd359
pman <pman-$-432894328923@gmail.com>
2024-08-23 10:58:33 +0200

    ...

--- run.sh
+++ run.sh
@@ -14,9 +14,9 @@ kubectl apply --server-side -f bundle.yaml
 
 kustomize build . | kubectl apply --server-side -f -
 
-sleep 10
+# sleep 10
 
-kubectl rollout status statefulset prometheus-prometheus --timeout=600s
+# kubectl rollout status statefulset prometheus-prometheus --timeout=600s
 # kubectl wait --for=condition=ready pod -l app.kubernetes.io/name=prometheus --timeout 300s
 
 # kubectl port-forward svc/prometheus-operated 9090:9090 > /dev/null 2>&1 &
